6be0d16eb0d9103be74221839f5c705325fd8534
zalikasetiawan07-maker <zalikasetiawan07@gmail.com>
2025-11-06 17:06:28 +0700

    baru

---
+++ fstream1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    // fstream file("text.txt");
+
+    // file.open("text.txt", ios::out);
+    // file << "yahaahhaahhah" << endl;
+
+    // file.close();
+
+    // file.open("text.txt", ios::in);
+    // string isi;
+
+    // while(getline(file, isi)){
+    //     cout << isi << endl;
+    // }
+
+    // fstream file("tex.txt");
+
+    // if(file.fail()){
+    //     cout << "gagal membuka file";
+    // } else {
+    //     cout << "berhasil buka file";
+    // }
+
+    // file.close();
+
+    // fstream myfile("text.txt", ios:: in);
+
+    // char isi;
+    // string isiLengkap = "";
+    // while(!myfile.eof())
+    // {
+    //     myfile.get(isi);
+    //     isiLengkap = isi;
+    // }
+
+    // cout << isiLengkap;
+
+    // myfile.close();
+
+    return 0;
+}
